c6e5a79387cb823f988adab45f848d5ca5ebec82
mohamed-mohaimen <mohamedveron23@gmail.com>
2021-03-06 02:08:12 +0200

    import geolocation service client

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
-import geolocation_service_interface "github.com/mohamedveron/geolocation-service/client"
+import (
+	"context"
+	"fmt"
+	geolocation_service_interface "github.com/mohamedveron/geolocation-service/client"
+	"net/http"
+)
 
 func main() {
 
-	//ip := "82.239.152.199"
-geolocation_service_interface.NewClient("http://localhost:8080")
+	ip := "82.239.152.199"
+	client2 , _ := geolocation_service_interface.NewClient("http://localhost:8080")
+
+	payload := geolocation_service_interface.GetLocationsJSONRequestBody{}
+	payload.IpAddress = &ip
+
+	ctx := context.Background()
+
+	resp, err := client2.GetLocations(ctx, payload)
+
+	if err != nil {
+		fmt.Print("failed to get locations", err)
+	} else if resp.StatusCode != http.StatusOK {
+		fmt.Print("failed to get locations", err)
+	}
+
+	parseRes, err := geolocation_service_interface.ParseGetLocationsResponse(resp)
+
+	fmt.Print(parseRes.JSON200.Locations)
 
 }
